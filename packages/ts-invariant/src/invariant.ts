3153bc7cf4fc7408e9455745be37cedde42119c6
Ben Newman <ben@apollographql.com>
2021-07-12 12:07:14 -0400

    Provide invariant.debug as wrapper for console.debug.
    Messages logged with invariant.debug will be hidden by default, but can
    be exposed by calling setVerbosity("debug").
    
    This additional log-level flexibility should be useful for addressing
    https://github.com/apollographql/apollo-client/issues/8442#issuecomment-877720942

--- packages/ts-invariant/src/invariant.ts
+++ packages/ts-invariant/src/invariant.ts
@@ -28,20 +28,24 @@ export function invariant(
   }
 }
 
-const verbosityLevels = ["log", "warn", "error", "silent"] as const;
-type VerbosityLevel = (typeof verbosityLevels)[number];
-type ConsoleMethodName = Exclude<VerbosityLevel, "silent">;
+const verbosityLevels = ["debug", "log", "warn", "error", "silent"] as const;
+export type VerbosityLevel = (typeof verbosityLevels)[number];
+export type ConsoleMethodName = Exclude<VerbosityLevel, "silent">;
 let verbosityLevel = verbosityLevels.indexOf("log");
 
-function wrapConsoleMethod<M extends ConsoleMethodName>(method: M) {
+function wrapConsoleMethod<M extends ConsoleMethodName>(name: M) {
   return function () {
-    if (verbosityLevels.indexOf(method) >= verbosityLevel) {
-      return console[method].apply(console, arguments as any);
+    if (verbosityLevels.indexOf(name) >= verbosityLevel) {
+      // Default to console.log if this host environment happens not to provide
+      // all the console.* methods we need.
+      const method = console[name] || console.log;
+      return method.apply(console, arguments as any);
     }
   } as (typeof console)[M];
 }
 
 export namespace invariant {
+  export const debug = wrapConsoleMethod("debug");
   export const log = wrapConsoleMethod("log");
   export const warn = wrapConsoleMethod("warn");
   export const error = wrapConsoleMethod("error");
